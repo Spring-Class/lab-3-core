d9ec70f161ed9f07915c06594383f5721c6b550c
Dmitry Shishagin <dmitry.shishagin@mail.ru>
2020-04-21 21:50:50 +0400

    exercise3: part1 is complited

--- src/main/java/com/luxoft/springioc/lab3/model/Person.java
+++ src/main/java/com/luxoft/springioc/lab3/model/Person.java
@@ -1,5 +1,7 @@
 package com.luxoft.springioc.lab3.model;
 
+import org.springframework.stereotype.Service;
+
 public interface Person {
     public void setName(String name);
     public String getName();
